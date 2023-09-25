08654578a69dbf51cbe9bd367a0bcfa483708063
profclovesrocha <clovesoficial@gmail.com>
2023-09-25 09:06:07 -0300

    Aula 7 JS
    - Seção 3: Funções

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Demonstração JS
+var chapter = 'SCOPE in JavaScript';
+function Pessoa() {
+
+var nome = 'Exemplo in function';
+//ESCREVA UMA FUNÇÃO PARA O SEU PROJETO
+function novoCadastro() {
+  sender.style.color = 'red';
+  console.log(chapter);
+  console.log(nome);
+  console.log(email);
+}
+novoCadastro();
+  
